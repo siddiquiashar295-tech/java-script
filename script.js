3b5342726021d29669508acdd00f6e35d6d8b55b
siddiquiashar295 <siddiquiashar295@gmail.com>
2026-02-19 12:53:14 +0500

    update

--- script.js
+++ script.js
@@ -1,16 +1,97 @@
-// console.log("Hello, World!");
+// chapter=1
 
-// console.log("This is a JavaScript file.");
+// QUESTION=1
+// alert("Welcome to JavaScript!");
 
-// console.log("my name is ashar")
+// QUESTION=2
+// alert("lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor."); 
+ 
+// question=3
+// alert("Welcome to JavaScript!\nHappy Coding!");
 
-// // fullname = "ashar siddiqui";
-// Age = "20";
-// console.log(Age);
+// QUESTION=4
+// alert("Welcome to JavaScript!");
+// alert("Happy Coding!");
 
 
-// alert("Welcome to my website!");
+// // CHAPTER=2
 
+// // QUESTION=1
+// var firstName = "John";
 
-alert("This is an alert message. Please click OK to continue.");
+// // QUESTION=2
+// var lastName = "Doe";
 
+// // QUESTION=3
+// var fullName = firstName + " " + lastName;
+
+// // QUESTION=4
+// alert(fullName);
+
+// QUESTION=5
+// studentName = "John Doe";
+// studentAge = 15;
+// studentCourse = "Certified Mobile Application Development";
+// alert("Name: " + studentName);
+// alert("Age: " + studentAge);
+// alert("Course: " + studentCourse);
+
+// question=6
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza);
+
+// question=7
+// var email = "myemail@example.com";
+// alert("My email address is " + email);  
+
+// question=8
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the book " + book);
+
+// CHAPTER=3
+
+// QUESTION=1
+// var age = 25;
+// alert("I am " + age + " years old");
+
+// QUESTION=2   
+// var visitCount = 14;
+// alert("You have visited this site " + visitCount + " times");
+
+// QUESTION=3   
+// var birthYear = 1990;
+// document.write("My birth year is " + birthYear + "<br>");
+// document.write("Data type of my declared variable is number");
+
+// // QUESTION=4
+// var visitorName = "John Doe";
+// var productTitle = "T-shirt";
+// var quantity = 5;
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+
+CHAPTER=4
+
+// QUESTION=1
+    // Var, let, const;
+
+// // QUESTION=2
+// Legal variable names:
+// var myVariable;
+// var _myVariable;
+// var $myVariable;
+// let variable123;
+// const myVariableName;
+
+// Illegal variable names:
+// var 123variable;
+// var my-variable;
+// var my variable;
+// letyVariable;
+// const myVariable!;
+
+// // QUESTION=3   
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("Variable names can only contain letters, numbers, underscores, and dollar signs. For example: $my_1stVariable<br>");
+// document.write("Variables must begin with a letter, underscore, or dollar sign. For example: $name, _name, or name<br>");
+// document.write("Variable names are case sensitive.<br>");
+// document.write("Variable names should not be JS keywords.");
