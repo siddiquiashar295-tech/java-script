ec1c6e5f87d9b339c39aaf58b2d25c357c6dd499
siddiquiashar295 <siddiquiashar295@gmail.com>
2026-02-19 05:16:21 +0500

    java intro

---
+++ script.js
@@ -0,0 +1,16 @@
+// console.log("Hello, World!");
+
+// console.log("This is a JavaScript file.");
+
+// console.log("my name is ashar")
+
+// // fullname = "ashar siddiqui";
+// Age = "20";
+// console.log(Age);
+
+
+// alert("Welcome to my website!");
+
+
+alert("This is an alert message. Please click OK to continue.");
+
