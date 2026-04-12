0056643f5745a7741f150090c1d5c121e614a84e
siddiquiashar295-tech <siddiquiashar295@gmail.com>
2026-04-12 15:04:09 +0500

    ramadan, Min or Max Logic

---
+++ logic.js
@@ -0,0 +1,40 @@
+
+
+// NUMBER
+
+
+// const numbers = [5, 10, 3, 89, 1];
+// const maxNumber = Math.max(...numbers);
+// console.log("The maximum number is:", maxNumber);
+
+
+// const numbers = [5, 10, 3, 89, 1, 0.5, 0.1];
+// const minNumber = Math.min(...numbers);
+// console.log("The minimum number is:", minNumber);
+
+
+//  PASSWORD
+
+// function generatePassword(length = 12) {
+//     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+//     let password = '';
+//     for (let i = 0; i < length; i++) {
+//         const randomIndex = Math.floor(Math.random() * characters.length);
+//         password += characters[randomIndex];
+//     }
+//     return password;
+// }       
+// const newPassword = generatePassword(16);
+// console.log("Generated Password:", newPassword);
+
+// RAMADAN
+
+// function calculateRamadanDates(year) {
+//     const ramadanStart = new Date(year, 1, 18); 
+//     const ramadanEnd = new Date(year, 2, 19); 
+//     return { start: ramadanStart, end: ramadanEnd };
+// }
+// const ramadanDates = calculateRamadanDates(2026);
+// console.log(`Ramadan starts on: ${ramadanDates.start.toDateString()}`);
+// console.log(`Ramadan ends on: ${ramadanDates.end.toDateString()}`);
+
